4aa0a5b39ae46f0f4fdabf96f73fd1b71ea7bfcf
widoc84 <sysadmin84@gmail.com>
2018-07-27 17:32:00 +0300

    test

--- testscript.py
+++ testscript.py
@@ -1,1 +1,9 @@
 
+from pywinauto import Application
+
+app = Application().start("C:\\Accord.x64\\aced32.exe")
+app.window(title=u'Aced32').wait('visible',timeout=3)
+app.window(title=u'Aced32')[u"Да"].DoubleClick()
+app.window(best_match="TMainForm").wait('visible',timeout=20)
+aced32 = app.window(best_match="TMainForm")
+acedtree = aced32.TreeView   
