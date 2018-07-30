b93bdfbcc07555de4b92fcc604c2d33e98218846
widoc84 <sysadmin84@gmail.com>
2018-07-30 17:54:44 +0300

    Update testscript.py

--- testscript.py
+++ testscript.py
@@ -1,3 +1,18 @@
+from pywinauto import Application
 import os
+import time
+import datetime
 
-os.mkdir("C:\\script\\test2\\")
+try:
+    app = Application().start("C:\\Accord.x64\\Aced32.exe")
+    print("20 sec")
+    app.window(best_match="ACED32 Редактор базы ", class_name='TMainForm').wait('visible',timeout=20)
+    app.kill()
+    print("Aced32 корректно открылся")
+    f.write("Aced32 корректно открылся\n\n")
+except:
+    print("Aced32 не открылся")
+    f.write("Aced32 не открылся\n\n")
+    app.kill()
+    result = 0
+time.sleep(5)
